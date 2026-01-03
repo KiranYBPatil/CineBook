652534ae0233ffcc9cb21610f547dff717be498c
KiranYBPatil <kiranpatil8660519@gmail.com>
2026-01-03 18:57:19 +0530

    Update axiosWrapper.js

--- bms-frontend/src/apis/axiosWrapper.js
+++ bms-frontend/src/apis/axiosWrapper.js
@@ -1,15 +1,16 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:9000/api/v1",
+  baseURL: import.meta.env.VITE_BACKEND_URL,
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
   },
 });
 
+// Attach token if present
 api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token"); // SAME KEY
+  const token = localStorage.getItem("token");
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
